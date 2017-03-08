6e4dafbd2fe477ba2792acdaa62a8086ceb87283
Marco Romano <marcojulioromano@gmail.com>
2017-03-08 17:26:34 +0100

    WIP...

---
+++ app/src/main/java/net/kjulio/torch/torch/flashlight/FlashlightTorchHelperFragment.java
@@ -0,0 +1,17 @@
+package net.kjulio.torch.torch.flashlight;
+
+import net.kjulio.torch.torch.TorchHelperFragment;
+
+public class FlashlightTorchHelperFragment extends TorchHelperFragment {
+
+    @Override
+    public void on() {
+
+    }
+
+    @Override
+    public void off() {
+
+    }
+
+}
